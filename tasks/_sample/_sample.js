57fcc81c94eb48c3b8165f58ab403a4328a9cb25
Taras Bobak <tbobak@tbobak-wf.hw.griddynamics.net>
2018-10-12 17:58:35 +0300

    Added eslint to prepush hook.

--- tasks/_sample/_sample.js
+++ tasks/_sample/_sample.js
@@ -6,22 +6,23 @@
  * @return {*}
  */
 export function binarySearch(array, value) {
-    let guess,
-        min = 0,
-        max = array.length - 1;
+  let guess;
+  let min = 0;
+  let max = array.length - 1;
 
-    while (min <= max) {
-        guess = Math.floor((min + max) / 2);
+  while (min <= max) {
+    guess = Math.floor((min + max) / 2);
 
-        if (array[guess] === value)
-            return guess;
-
-        if (array[guess] < value) {
-            min = guess + 1;
-        } else {
-            max = guess - 1;
-        }
+    if (array[guess] === value) {
+      return guess;
     }
 
-    return -1;
+    if (array[guess] < value) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
+    }
+  }
+
+  return -1;
 }
